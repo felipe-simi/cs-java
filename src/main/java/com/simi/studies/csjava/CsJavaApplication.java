1c25799473f4a923e7774c28612f5eacaa5f7969
Felipe Miranda <felip.simi@gmail.com>
2020-07-23 22:00:31 -0300

    Preparacao: setup do projeto no Spring Initializr

---
+++ src/main/java/com/simi/studies/csjava/CsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.simi.studies.csjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simi/studies/csjava/CsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simi.studies.csjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
