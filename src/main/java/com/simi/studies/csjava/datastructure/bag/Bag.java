ec71fa47e6614262240d7d39b09d7a917738a530
Felipe Miranda <felip.simi@gmail.com>
2020-08-15 17:47:06 -0300

    Bag: controle do tamanho

--- src/main/java/com/simi/studies/csjava/datastructure/bag/Bag.java
+++ src/main/java/com/simi/studies/csjava/datastructure/bag/Bag.java
@@ -3,6 +3,7 @@ package com.simi.studies.csjava.datastructure.bag;
 public class Bag<T> {
 
   private Node<T> first;
+  private int totalElements;
 
   private static class Node<T> {
     private T element;
@@ -12,10 +13,15 @@ public class Bag<T> {
   public void add(T t) {
     first = new Node();
     first.element = t;
+    totalElements++;
   }
 
   public boolean isEmpty() {
     return first == null;
+  }
+
+  public int size() {
+    return totalElements;
   }
 
 }
--- src/test/java/com/simi/studies/csjava/datastructure/bag/BagTest.java
+++ src/test/java/com/simi/studies/csjava/datastructure/bag/BagTest.java
@@ -6,16 +6,24 @@ import org.junit.jupiter.api.Test;
 class BagTest {
 
   @Test
-  public void givenBagWithoutElements_whenCheckEmptiness_thenShouldBeTrue() {
+  public void givenBagWithoutElements_whenCheckingEmptiness_thenShouldBeTrue() {
     final var emptyBag = new Bag();
     Assertions.assertTrue(emptyBag.isEmpty());
   }
 
   @Test
-  public void givenBagWithOneElement_whenCheckEmptiness_thenShouldBeFalse() {
-    final var bag = new Bag();
+  public void givenBagWithOneElement_whenCheckingEmptiness_thenShouldBeFalse() {
+    final var bag = new Bag<String>();
     bag.add("Test");
     Assertions.assertFalse(bag.isEmpty());
+  }
+
+  @Test
+  public void givenBagWithMultipleElements_whenCheckingSize_thenShouldTwo() {
+    final var bag = new Bag<String>();
+    bag.add("Test1");
+    bag.add("Test2");
+    Assertions.assertEquals(2, bag.size());
   }
 
 }
