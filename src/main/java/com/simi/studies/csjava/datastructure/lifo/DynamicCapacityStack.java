c1c6e225e059b3dd62192a106ab35cda27cc8392
Felipe Miranda <felip.simi@gmail.com>
2020-08-19 21:04:51 -0300

    Stack: adicao de elementos da lista com tamanho dinamico

--- src/main/java/com/simi/studies/csjava/datastructure/lifo/DynamicCapacityStack.java
+++ src/main/java/com/simi/studies/csjava/datastructure/lifo/DynamicCapacityStack.java
@@ -5,6 +5,23 @@ import java.util.Optional;
 
 public class DynamicCapacityStack<T> implements Stack<T> {
 
+  private T[] elements;
+  private int totalElements;
+  private final float loadFactor;
+
+  public DynamicCapacityStack(final int initialCapacity) {
+    this(initialCapacity, 0.75f);
+  }
+
+  public DynamicCapacityStack(final float loadFactor) {
+    this(10, loadFactor);
+  }
+
+  public DynamicCapacityStack(final int initialCapacity, final float loadFactor) {
+    this.loadFactor = loadFactor;
+    this.elements = (T[]) new Object[initialCapacity];
+  }
+
   private void decreaseCapacity() {
   }
 
@@ -13,7 +30,8 @@ public class DynamicCapacityStack<T> implements Stack<T> {
 
   @Override
   public void push(T item) {
-
+    elements[totalElements] = item;
+    totalElements++;
   }
 
   @Override
@@ -23,7 +41,7 @@ public class DynamicCapacityStack<T> implements Stack<T> {
 
   @Override
   public boolean isEmpty() {
-    return true;
+    return totalElements == 0;
   }
 
   @Override
--- src/test/java/com/simi/studies/csjava/datastructure/lifo/DynamicCapacityStackTest.java
+++ src/test/java/com/simi/studies/csjava/datastructure/lifo/DynamicCapacityStackTest.java
@@ -7,8 +7,15 @@ class DynamicCapacityStackTest {
 
   @Test
   public void givenStackWithoutElements_whenCheckingEmptiness_thenShouldBeTrue() {
-    final var emptyStack = new DynamicCapacityStack<String>();
+    final var emptyStack = new DynamicCapacityStack<String>(1);
     Assertions.assertTrue(emptyStack.isEmpty());
+  }
+
+  @Test
+  public void givenStackWithOneElement_whenCheckingEmptiness_thenShouldBeFalse() {
+    final var fixedCapacity = new DynamicCapacityStack<String>(1);
+    fixedCapacity.push("Test");
+    Assertions.assertFalse(fixedCapacity.isEmpty());
   }
 
 }
