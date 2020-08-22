ba3c8e1bf4f943fd42f8a6fa59e4aedc93219bb5
Felipe Miranda <felip.simi@gmail.com>
2020-08-21 21:26:50 -0300

    Queue: criacao da classe para lista unida individualmente

---
+++ src/test/java/com/simi/studies/csjava/datastructure/fifo/SinglyLinkedQueueTest.java
@@ -0,0 +1,14 @@
+package com.simi.studies.csjava.datastructure.fifo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SinglyLinkedQueueTest {
+
+  @Test
+  public void givenQueueWithoutElements_whenCheckingEmptiness_thenShouldBeTrue() {
+    final var emptyQueue = new SinglyLinkedQueue<String>();
+    Assertions.assertTrue(emptyQueue.isEmpty());
+  }
+
+}
