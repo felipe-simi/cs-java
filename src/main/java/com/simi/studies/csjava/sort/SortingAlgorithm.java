26563c4940717b9314c54cad8327bbad3c1c49db
Felipe Miranda <felip.simi@gmail.com>
2020-10-18 11:58:17 -0300

    Ordenacao: metodos padroes

--- src/main/java/com/simi/studies/csjava/sort/SortingAlgorithm.java
+++ src/main/java/com/simi/studies/csjava/sort/SortingAlgorithm.java
@@ -8,4 +8,20 @@ public interface SortingAlgorithm<T extends Comparable> {
 
   T[] sort(T[] elements, Comparator comparator);
 
+  default boolean isSmallerThan(T nextElement, T currentElement, Comparator comparator) {
+    if (currentElement == null || nextElement == null) {
+      throw new IllegalArgumentException("An array containing null element cannot be sorted.");
+    }
+    if (comparator == null) {
+      return nextElement.compareTo(currentElement) < 0;
+    }
+    return comparator.compare(nextElement, currentElement) < 0;
+  }
+
+  default void swapElements(T[] elements, int nextElement, int currentElement) {
+    T temp = elements[nextElement];
+    elements[nextElement] = elements[currentElement];
+    elements[currentElement] = temp;
+  }
+
 }
